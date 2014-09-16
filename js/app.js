5ef03ad0fa6f87d80bb102b818dc117a29e7042a
Tanvir Aslam <tanviraslam@outlook.com>
2014-09-16 14:29:48 -0700

    Added search bar, can retrieve pictures

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var app = angular.module("app", ['ngAnimate']);
+
+app.controller("InputController", function($scope, $http){
+  $scope.submit = function(){
+    $scope.tag = $scope.data.tag;
+    $scope.data.tag="";
+
+    //Make the Ajax call to Instagram and get back the results
+    $http({
+      url: 'https://api.instagram.com/v1/tags/' + $scope.tag + '/media/recent',
+      method: 'JSONP',
+      params: {
+        callback : 'JSON_CALLBACK',
+        client_id : '3c1e3c03496f41b49ff28840d7d8e3df'
+      }
+    })
+    .success(function(images){
+      console.log(images.data);
+      $scope.images = images.data;
+      $scope.imageRetrieved = true;
+    })
+    .error(function(data){
+      console.log(data);
+    });
+  }
+
+
+ 
+});
